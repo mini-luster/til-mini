45adb23ad8f2bacbf0a408ecf18529e6dfbf9b60
minhee <43980992+minheeyoon@users.noreply.github.com>
2020-10-23 20:38:19 +0900

    UPDATE-201023
    UPDATE-201023

---
+++ UPDATE/201022_JS공부_과제/index.js
@@ -0,0 +1,13 @@
+document.write("test")
+
+const students = [
+    {
+      name : "코난",
+      grade: [
+        {subject: 'maths', score:92},
+        {subject: 'english', score:92},
+        {subject: 'society', score:78},
+        {subject: 'science', score:72},
+        {subject: 'korean', score:88}
+      ]
+    }, ]
