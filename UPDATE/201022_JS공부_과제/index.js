4103ded3f1becd505cc2aa776d2fa222f71b67fd
윤민희 <mini940511@gmail.com>
2020-10-25 20:19:22 +0900

    아 진짜 모르겠음
    아 진짜 모르겠음

--- UPDATE/201022_JS공부_과제/index.js
+++ UPDATE/201022_JS공부_과제/index.js
@@ -1,8 +1,17 @@
+// form.addEventListener('.style__input__btn', function callBack(e)
+// {
+
+// });
 
 
+// const input = document.querySelector (".style__input")
 
-for (let i = 1; i<=9; i++) {
-    console.log(2 + "x" + i + "=" + 2*i )
-};
+// document.getElementById("submit").onclick = function() {
+// }
+// const 
+
+// for (let i = 1; i<=9; i++) {
+//     console.log(2 + "x" + i + "=" + 2*i )
+// };
 
 
