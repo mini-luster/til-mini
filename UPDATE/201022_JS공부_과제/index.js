7e7ea6ff30993dcd14f6ccac60a76464484025d6
minhee <43980992+minheeyoon@users.noreply.github.com>
2020-10-25 23:20:46 +0900

    UPDATE - 201025
    UPDATE - 201025

--- UPDATE/201022_JS공부_과제/index.js
+++ UPDATE/201022_JS공부_과제/index.js
@@ -1,12 +1,31 @@
-// form.addEventListener('.style__input__btn', function callBack(e)
-// {
+// function gugudan(){
+//     let gogoInput = document.getElementById('input').value;
+//     let temp = "";
+//     let result = "";
 
-// });
+//     for (i = 0; i < 10; i++) {
+//         result = guguInput * i;
+//         temp += guguInput + "X" + i + "=" + result;
+//     }
+
+//     document.getElementById('contents').innerHTML = temp;
+  
+// }
 
 
 // const input = document.querySelector (".style__input")
 
-// document.getElementById("submit").onclick = function() {
+// document.getElementById("submit").onclick = function gogodan(){
+//     let gogoInput = document.getElementById('input').value;
+//     let temp = "";
+//     let result = "";
+
+//     for (i = 0; i < 10; i++) {
+//         result = guguInput * i;
+//         temp += guguInput + "X" + i + "=" + result;
+//     }
+
+//     document.getElementById('contents').innerHTML = temp;
 // }
 // const 
 
@@ -15,3 +34,27 @@
 // };
 
 
+
+
+// 
+
+window.addEventListener("load", function(){
+
+    let numeric = document.getElementById("input-gugudan");
+    let btn = document.getElementById("gugudan-btn");
+
+    btn.onclick = function(){
+        numeric = parseInt(numeric.value);
+
+           
+           if ( numeric >= 10 ) {
+               document.write("어허, 구구단이니까 10아래 숫자로만 해주라")
+           } else {
+                 for (let i = 1; i<=9; i++) {
+                    document.write( numeric + "x" + i + "=" + numeric * i + "//" )
+                };
+           }
+
+    }
+
+})
