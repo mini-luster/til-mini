b8f6ed86c8f697da2864a39bf33ce6aff1776eb6
minhee <43980992+minheeyoon@users.noreply.github.com>
2020-10-27 00:08:25 +0900

    이든과함께
    이든과함께

--- UPDATE/201022_JS공부_과제/index.js
+++ UPDATE/201022_JS공부_과제/index.js
@@ -47,13 +47,26 @@ window.addEventListener("load", function(){
         numeric = parseInt(numeric.value);
 
            
-           if ( numeric >= 10 ) {
-               document.write("어허, 구구단이니까 10아래 숫자로만 해주라")
-           } else {
-                 for (let i = 1; i<=9; i++) {
-                    document.write( numeric + "x" + i + "=" + numeric * i + '<br>' )
-                };
-           }
+        if ( isNaN(numeric) ) {
+            document.write("어허, 숫자만써주세요");
+        } else if ( numeric < 2 || numeric > 10 ) {
+            document.write("어허, 구구단이니까 2에서 9까지의 숫자로만 해주라");
+        } else {
+            for (let i = 1; i<=9; i++) {
+                document.write( numeric + "x" + i + "=" + numeric * i + '<br>' );
+            };
+        }
+
+        //    if ( numeric >= 10 ) {
+        //        document.write("어허, 구구단이니까 10아래 숫자로만 해주라")
+        //    } else if(isNaN(numeric)){
+        //      document.write("어허, 숫자만써주세요")
+        //    }
+        //    else {
+        //          for (let i = 1; i<=9; i++) {
+        //             document.write( numeric + "x" + i + "=" + numeric * i + '<br>' )
+        //         };
+        //    }
 
     }
 
