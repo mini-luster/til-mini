a19bca8f9739a18ebf7ad219d3f3625807d59362
윤민희 <mini940511@gmail.com>
2020-12-26 23:21:49 +0900

    test
    tset

--- UPDATE/201215_JS공부_정리/index.js
+++ UPDATE/201215_JS공부_정리/index.js
@@ -1,18 +1,52 @@
-function func1() {
-  console.log('func1');
-  func2();
-}
+let params = new URL(document.location).searchParams;
+let idx = +params.get("idx");
+let chapter = +params.get("chapter");
+let prevProgress = 0;
+let slideNum = 1;
+let lectureInterval = setInterval(() => {
+  if (chapter > 3) {
+    clearInterval(lectureInterval);
+    return;
+  }
+  console.log("idx", idx, "Chapter", chapter);
+  $.ajax({
+    url:
+      `${location.protocol}//${location.host}/app/api/study/chapter_jindo.asp`,
+    dataType: "json",
+    data: {
+      idx,
+      chapter,
+      slideNum,
+      slideUrl: "",
+      slideCount: 7,
+    },
+    xhrFields: {
+      withCredentials: true,
+    },
+    success: (data) => {
+      if (data?.result !== "success") {
+        slideNum = 1;
+        return;
+      }
+      console.log("현재 진행률", data.chapter.study_ok);
+      if (data?.chapter?.study_ok === prevProgress) {
+        slideNum += 1;
+      }
+      if (data?.chapter?.study_ok === 100) {
+        chapter += 1;
+        slideNum = 1;
+        prevProgress = 0;
+      }
+      prevProgress = +(data?.chapter?.study_ok ?? 0);
+    },
+    error: () => {
+      clearInterval(lectureInterval);
+    },
+  });
+}, 2000);
+Collapse
 
-function func2() {
-  setTimeout(function () {
-    console.log('func2');
-  }, 0);
 
-  func3();
-}
 
-function func3() {
-  console.log('func3');
-}
 
-func1();
+
